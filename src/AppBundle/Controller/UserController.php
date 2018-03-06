99839c6f9b79d730cfece4599d6c2deff63e0851
Unknown <ml.maroux@gmail.com>
2018-03-06 09:59:34 +0100

    add: user security, user controller, rooting

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Movie;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+
+}
